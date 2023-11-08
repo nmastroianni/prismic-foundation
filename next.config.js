ce4aa1ce9f06148e714b5621286c0ffca3ab6d5a
Neil Mastroianni <nf_mastroianni@hotmail.com>
2023-11-08 18:32:43 -0500

    added content and imagewithtext

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: [
+      'images.prismic.io',
+      'images.unsplash.com',
+      'prismic-io.s3.amazonaws.com',
+    ],
+  },
+}
 
 module.exports = nextConfig
